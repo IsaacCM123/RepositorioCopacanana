cc12de9b66b30939f4335902d0ed925d84110f24
IsaacCM123 <condoriisaac06@gmail.com>
2026-05-29 21:41:30 -0400

    yyy

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ io.on('connection',(socket)=>{
 
                                                                 let transporter=nodemailer.createTransport({
                                                                   service:'gmail',
-                                                                  family:4/*Forzar IP solo para desplegar a render lo borramos, no afecta*/
+                                                                  family:4,/*Forzar IP solo para desplegar a render lo borramos, no afecta*/
                                                                   auth:{
                                                                     user:process.env.EMAIL_USER,
                                                                     pass:process.env.EMAIL_PASS
